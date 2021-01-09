5852dfb4922c6701fba98d17614695f2b430ec6f
Cherry-LY <1148688364@qq.com>
2021-01-09 23:48:10 +0800

    add c.cpp

---
+++ c.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+ int y=0;//declaration and assignment??????
+ int x;//declaration
+ x=1+1;//assighment   1+1的结果赋值给x 
+	/*This is a comment*/
+	//请计算1+1=2，并把结果显示出来 
+	//变量赋值前必须声明 
+	printf("To C,or not to C:\n");
+   printf("that is the question.\n");
+   printf("1+1=%d\n",1+1);//混合语句，先算出结果，再调用printf 
+   printf("1+1=%d\n",x);//x是变量 
+   printf("1+1=%d\n",2);//2是常量，显示 
+   
+	return 0;
+}
