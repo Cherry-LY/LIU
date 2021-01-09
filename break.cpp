56234c1d868591654f972094a76c0322c961a4dd
Cherry-LY <1148688364@qq.com>
2021-01-09 23:45:37 +0800

    add break.cpp

---
+++ break.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int n,d;
+	scanf("%d",&n); 
+	
+	for(d=2;d<n;d++)
+	   if (n%d==0)
+	   break;/*Ìø³öforÑ­»·*/
+	n 
+	if (d<n)
+	  printf("%d is divisible by %d\n",n,d);
+	else
+	  printf("%d is prime\n",n);
+
+    return 0;
+}
